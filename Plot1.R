6760c846105931d6e216c71aeba470ac4bcd3a74
Ahmed Shibrain <mr.shibrain@gmail.com>
2014-06-08 17:51:53 +0300

    Fix Plot 1

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,5 @@
 source('DataProcessor.R')
-EPCDate <- GetMyDataEPC()
+EPCData <- GetMyDataEPC()
 
 png(filename = "plot1.png",width = 480, height = 480)
 hist(EPCData$Global_active_power,main="Global Active Power",col="red",xlab="Global Active Power (kilowatts)")
