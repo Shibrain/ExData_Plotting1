05ed2659f0207e6846a22d933e5488c0c3664494
Ahmed Shibrain <mr.shibrain@gmail.com>
2014-06-08 17:51:33 +0300

    Fix Plot2

--- Plot2.R
+++ Plot2.R
@@ -1,5 +1,5 @@
 source('DataProcessor.R')
-EPCDate <- GetMyDataEPC()
+EPCData <- GetMyDataEPC()
 
 png(filename = "plot2.png",width = 480, height = 480)
 plot(EPCData$DateTime, EPCData$Global_active_power,type="l", xlab="", ylab="Global Active Power (kilowatts)")
